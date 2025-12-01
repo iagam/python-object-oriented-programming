cd41cfca4c5af42e6eb00b176f7e23ac85ee8edf
Agam <54039021+iagam@users.noreply.github.com>
2025-12-01 15:51:17 +0530

    Add methods for enemy

--- oop-section/enemy.py
+++ oop-section/enemy.py
@@ -1,6 +1,27 @@
 # V1 - Introduction to OOP - Create an enemy class
+# class Enemy:
+
+#     type_of_enemy: str
+#     health_points: int = 10
+#     attack_damage: int = 1
+
+# V2 - Abstraction
+# Abstarction means to hide implementation and only show necessary details to the user.
+# User doesn't need to know what's going under the hood.
+# In the long run helps make code scalable
+# Propoagates the DRY (Don't Repeat Yourself) principle
+
 class Enemy:
 
     type_of_enemy: str
     health_points: int = 10
     attack_damage: int = 1
+
+    def talk(self):
+        print(f"I am a {self.type_of_enemy}. Be prepared to fight.")
+
+    def walk_forward(self):
+        print(f"{self.type_of_enemy} moves closer to you.")
+
+    def attack(self):
+        print(f"{self.type_of_enemy} attacks for {self.attack_damage} damage.")
