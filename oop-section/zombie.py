5db8701e8a2c0e275452ad813d3bcb60b5166244
Agam <54039021+iagam@users.noreply.github.com>
2025-12-02 14:00:28 +0530

    Extend special_attack method

--- oop-section/zombie.py
+++ oop-section/zombie.py
@@ -1,13 +1,17 @@
 from enemy import *
+import random
 
 # Zombie as a child class of Enemy
 class Zombie(Enemy):
 
     def __init__(self, health_points, attack_damage):
         # Call Parent constructors using super
-        super().__init__(type_of_enemy = "Zombie",
-                         health_points = health_points,
-                         attack_damage = attack_damage)
+        super().__init__(
+            type_of_enemy="Zombie",
+            health_points=health_points,
+            attack_damage=attack_damage,
+            regeneration_points=2,
+        )
 
     # augmenting parent method
     def talk(self):
@@ -17,3 +21,10 @@ class Zombie(Enemy):
     def spread_disease(self):
         enemy_type = self.get_type_of_enemy()
         print(f'{enemy_type} is trying to spread diseases.')
+
+    def special_attack(self):
+        enemy_type = self.get_type_of_enemy()
+        did_special_attack_work = random.random()
+        if did_special_attack_work < 0.5:
+            self.health_points += self.regeneration_points
+            print(f"{enemy_type} generated {self.regeneration_points} HP")
